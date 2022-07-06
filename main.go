dedd8b5f76d65aee834fa0371bfc7761eddde23a
Liubomur Puliak <liubomurpuliak@gmail.com>
2022-07-06 23:08:00 +0300

    fix struct name

--- main.go
+++ main.go
@@ -4,53 +4,49 @@ import (
 	"fmt"
 )
 
-type Dog struct {
+type dog struct {
 	name            string  //ім'я
 	feedConsumption float64 //розхід корму
 	weight          float64 //вага
 }
 
-func (d Dog) consumption() float64 { //розрахунок кількості корму на місяць
+func (d dog) consumption() float64 { //розрахунок кількості корму на місяць
 	return d.feedConsumption * d.weight
 }
 
-func (d Dog) String() string {
+func (d dog) String() string {
 	return fmt.Sprintf("A %s weighs %.2f kilograms", d.name, d.weight)
 }
 
-type Cat struct {
+type cat struct {
 	name            string  //ім'я
 	feedConsumption float64 //розхід корму
 	weight          float64 //вага
 }
 
-func (c Cat) consumption() float64 { //розрахунок кількості корму на місяць
+func (c cat) consumption() float64 { //розрахунок кількості корму на місяць
 	return c.feedConsumption * c.weight
 }
 
-func (c Cat) String() string {
+func (c cat) String() string {
 	return fmt.Sprintf("A %s weighs %.2f kilograms", c.name, c.weight)
 }
 
-type Cov struct {
+type cov struct {
 	name            string  //ім'я
 	feedConsumption float64 //розхід корму
 	weight          float64 //вага
 }
 
-func (v Cov) consumption() float64 { //розрахунок кількості корму на місяць
+func (v cov) consumption() float64 { //розрахунок кількості корму на місяць
 	return v.feedConsumption * v.weight
 }
-func (v Cov) String() string {
+func (v cov) String() string {
 	return fmt.Sprintf("A %s weighs %.2f kilograms", v.name, v.weight)
 }
 
-type Feeder interface {
-	consumption() float64
-}
-
 type Constructer interface {
-	Feeder
+	consumption() float64
 	fmt.Stringer
 }
 
@@ -61,11 +57,11 @@ func PrintAll(c Constructer) {
 func main() {
 
 	dogConsumption := 10 / 2 //розрахунок кількості корму на один кілограм для собак
-	c := Cat{name: "murchuk", feedConsumption: 7, weight: 5}
+	c := cat{name: "murchuk", feedConsumption: 7, weight: 5}
 	PrintAll(c)
-	d := Dog{name: "barsik", feedConsumption: float64(dogConsumption), weight: 20}
+	d := dog{name: "barsik", feedConsumption: float64(dogConsumption), weight: 20}
 	PrintAll(d)
-	v := Cov{name: "munia", feedConsumption: 25, weight: 100}
+	v := cov{name: "munia", feedConsumption: 25, weight: 100}
 	PrintAll(v)
 	fmt.Println(c.consumption()+v.consumption()+d.consumption(), "kilograms of fodder are required per month")
 }
